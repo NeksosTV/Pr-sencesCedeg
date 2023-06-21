433883c7495372c046367b710f8ea8e0ecca43a1
NeksosTV <neksostv@outlook.fr>
2023-06-21 20:44:15 +0200

    delet function admin

--- components/AttendanceForm.vue
+++ components/AttendanceForm.vue
@@ -28,9 +28,6 @@ import PocketBase from 'pocketbase';
     },
     methods: {
       submitForm() {
-        // Logique pour soumettre le formulaire et envoyer les données à Pocketbase
-        
-        // Exemple de code pour envoyer les données à Pocketbase
         const formData = {
           name: this.name,
           email: this.email
@@ -40,27 +37,7 @@ import PocketBase from 'pocketbase';
         // Appeler la fonction d'envoi de données à Pocketbase (utilisez le code approprié de la documentation Pocketbase)
         this.sendFormDataToPocketbase(formData);
       },
-      sendFormDataToPocketbase(formData) {
-        // Code pour envoyer les données à Pocketbase
-        
-        // Exemple de code pour envoyer les données à Pocketbase en utilisant PocketBase SDK
-   
-        
-        const pb = new PocketBase('http://127.0.0.1:8090');
-        
-        // Authentifier en tant qu'administrateur (utilisez le code approprié de la documentation Pocketbase)
-        const authData =  pb.admins.authWithPassword('admin@example.com', 'adminpassword');
-        
-        // Envoyer les données à Pocketbase (utilisez le code approprié de la documentation Pocketbase)
-        const response =  pb.data.create('attendance', formData);
-        
-        // Traiter la réponse de Pocketbase si nécessaire
-        
-        // Réinitialiser les champs du formulaire
-        this.name = '';
-        this.email = '';
-        // Réinitialiser les autres champs du formulaire si nécessaire
-      }
+
     }
   };
   </script>
